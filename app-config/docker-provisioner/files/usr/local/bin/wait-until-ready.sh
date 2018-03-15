4f8a4759f6967a6f3b061d8244a49986498e373f
Jack Boespflug <jack.boespflug@kineticdata.com>
2018-03-14 22:43:37 -0500

    Changes for more reliable startup.

--- app-config/docker-provisioner/files/usr/local/bin/wait-until-ready.sh
+++ app-config/docker-provisioner/files/usr/local/bin/wait-until-ready.sh
@@ -1,16 +1,30 @@
 #!/usr/bin/env bash
 set -e
 
-host="$1"; shift
-port="$1"; shift
 cmd="$@"
 
 sleep 5
 
-until ! nc -z "$host" "$port"; do
-  >&2 echo "Port $host:$port is open - sleeping"
+until ! nc -z "configuration" "8181"; do
+  >&2 echo "Port configuration:8181 is open - sleeping"
+  sleep 3
+done
+until nc -z "request-ce" "8080"; do
+  >&2 echo "Port request-ce:8080 is closed - sleeping"
+  sleep 3
+done
+until nc -z "task" "8080"; do
+  >&2 echo "Port task:8080 is closed - sleeping"
+  sleep 3
+done
+until nc -z "bridgehub" "8080"; do
+  >&2 echo "Port bridgehub:8080 is closed - sleeping"
+  sleep 3
+done
+until nc -z "filehub" "8080"; do
+  >&2 echo "Port filehub:8080 is closed - sleeping"
   sleep 3
 done
 
->&2 echo "Port $host:$port is closed! Resuming execution."
+>&2 echo "Applications are ready! Resuming execution."
 exec $cmd
